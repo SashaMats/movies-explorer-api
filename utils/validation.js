721afbd18f8f46a05397826113b8e1386accfcf1
Alexandr Bokov <bok.off@gmail.com>
2023-10-28 10:36:25 +0300

    fix: reduct name and more

--- utils/validation.js
+++ utils/validation.js
@@ -7,18 +7,17 @@ module.exports.cardValidation = celebrate({
       .max(30),
     director: Joi.string().required().min(2)
       .max(30),
-    duration: Joi.string().hex().required().min(1)
-      .max(3),
+    duration: Joi.number(),
     year: Joi.string().hex().required().min(1)
       .max(4),
     description: Joi.string().required().min(2)
-      .max(250),
+      .max(1000),
     image: Joi.string().required().pattern(urlPattern),
     trailer: Joi.string().required().pattern(urlPattern),
     nameRu: Joi.string().required().min(2)
-      .max(30),
+      .max(60),
     nameEn: Joi.string().required().min(2)
-      .max(30),
+      .max(60),
     thumbnail: Joi.string().required().pattern(urlPattern),
     movieId: Joi.number().required(),
   }),
@@ -34,6 +33,14 @@ module.exports.emailNameUserValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     name: Joi.string().min(2).max(30),
+  }),
+});
+
+module.exports.nameEmailPassUserValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(3),
   }),
 });
 
