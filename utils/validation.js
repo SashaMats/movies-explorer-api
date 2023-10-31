74c43a27b04413d811c51e188d0de4dbf1978a6e
SashaMats <120097655+SashaMats@users.noreply.github.com>
2023-10-31 17:13:07 +0300

    Level 1 (#19)
    * first commit
    
    * second commit
    
    * my commit
    
    * work to  check
    
    * work to check 1
    
    * work check 2
    
    * work check 3
    
    * work to check 4
    
    * work to check 5
    
    * fix: reduct name and more
    
    * fix: movie validation and error text reducting

--- utils/validation.js
+++ utils/validation.js
@@ -1,28 +1,44 @@
 const { celebrate, Joi } = require('celebrate');
-const urlPattern = require('./constants');
+
+// const urlPattern = require('./constants');
 
 module.exports.cardValidation = celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required().min(2)
-      .max(30),
-    director: Joi.string().required().min(2)
-      .max(30),
-    duration: Joi.string().hex().required().min(1)
-      .max(3),
-    year: Joi.string().hex().required().min(1)
-      .max(4),
-    description: Joi.string().required().min(2)
-      .max(250),
-    image: Joi.string().required().pattern(urlPattern),
-    trailer: Joi.string().required().pattern(urlPattern),
-    nameRu: Joi.string().required().min(2)
-      .max(30),
-    nameEn: Joi.string().required().min(2)
-      .max(30),
-    thumbnail: Joi.string().required().pattern(urlPattern),
-    movieId: Joi.number().required(),
+    country: Joi.string(),
+    director: Joi.string(),
+    duration: Joi.number(),
+    year: Joi.string(),
+    description: Joi.string(),
+    image: Joi.string(),
+    trailer: Joi.string(),
+    nameRu: Joi.string(),
+    nameEn: Joi.string(),
+    thumbnail: Joi.string(),
+    movieId: Joi.number(),
   }),
 });
+
+// module.exports.cardValidation = celebrate({
+//   body: Joi.object().keys({
+//     country: Joi.string().required().min(2)
+//       .max(30),
+//     director: Joi.string().required().min(2)
+//       .max(30),
+//     duration: Joi.number(),
+//     year: Joi.string().hex().required().min(1)
+//       .max(4),
+//     description: Joi.string().required().min(2)
+//       .max(50),
+//     image: Joi.string().required().pattern(urlPattern),
+//     trailer: Joi.string().required().pattern(urlPattern),
+//     nameRu: Joi.string().required().min(2)
+//       .max(10),
+//     nameEn: Joi.string().required().min(2)
+//       .max(60),
+//     thumbnail: Joi.string().required().pattern(urlPattern),
+//     movieId: Joi.number().required(),
+//   }),
+// });
 
 module.exports.idValidation = celebrate({
   params: Joi.object().keys({
@@ -34,6 +50,14 @@ module.exports.emailNameUserValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     name: Joi.string().min(2).max(30),
+  }),
+});
+
+module.exports.nameEmailPassUserValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(3),
   }),
 });
 
